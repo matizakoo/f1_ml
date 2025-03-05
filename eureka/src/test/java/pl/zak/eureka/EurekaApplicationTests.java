f48c8d517938329880a47b58137a68a94a753c16
test.ta <matizakoo@gmail.com>
2025-03-05 16:22:10 +0100

    init

---
+++ eureka/src/test/java/pl/zak/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zak.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
