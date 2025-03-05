d8f527fc1fd1a408ad902ad18e92587ae9e45dae
test.ta <matizakoo@gmail.com>
2025-03-05 22:56:39 +0100

    init

---
+++ auth/src/main/java/pl/zak/auth/Controller.java
@@ -0,0 +1,13 @@
+package pl.zak.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping()
+    public String hello() {
+        return "Hello World";
+    }
+}
