661f1daa0b0417c8308584d4ad1ebbbb93033115
matizakoo <matizakoo@gmail.com>
2025-03-16 02:09:31 +0100

    login page without service impl

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule, provideHttpClient} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
+    provideHttpClient()
   ],
   bootstrap: [AppComponent]
 })
--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {LoginService} from "./login.service";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string = '';
 
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  public get email() {
+    return this.loginForm.get('email');
+  }
+
+  public get password() {
+    return this.loginForm.get('password');
+  }
+
+  getEmailErrorMessage(): string {
+    if (this.email?.hasError('required')) {
+      return 'Email jest wymagany';
+    }
+    if (this.email?.hasError('email')) {
+      return 'Podaj poprawny adres email';
+    }
+    return '';
+  }
+
+  getPasswordErrorMessage(): string {
+    if (this.password?.hasError('required')) {
+      return 'Hasło jest wymagane';
+    }
+    if (this.password?.hasError('minlength')) {
+      return 'Hasło musi mieć co najmniej 6 znaków';
+    }
+    return '';
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.loginService.login(email, password).subscribe({
+        next: (response) => {
+          console.log('Zalogowano!', response);
+        },
+        error: (err) => {
+          console.error('Błąd logowania', err);
+          this.loginError = 'Niepoprawny email lub hasło';
+        }
+      });
+    }
+  }
 }
